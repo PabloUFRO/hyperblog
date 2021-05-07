ac484983a2ec9b6d9f73e801c5f580aa5f043799
Pablo Sanhueza <sanhueza.carvajal.p@gmail.com>
2021-05-07 09:30:36 -0400

    Change to english language (good practice)

--- L298N.py
+++ L298N.py
@@ -1,11 +1,12 @@
 from machine import Pin, PWM
 from time import sleep
 
-#DEFINICIÓN DE PINES ESP32 DEBKITV1 PARA PUENTE H
-#PINOUT = https://drive.google.com/file/d/1bQJsxdrHbP_0fmOiLdE5OCmChhQcu7oR/view?usp=sharing
-#H-BRIDGE = https://drive.google.com/file/d/1--dudUWrGzpyWKEEIy7zS6vQjkBpHAVd/view?usp=sharing
+# uPython library to control motors with the H-bridge L298N and ESP32 DEBKITV1
+ 
+#ESP32 PINOUT = https://drive.google.com/file/d/1bQJsxdrHbP_0fmOiLdE5OCmChhQcu7oR/view?usp=sharing
+#H-BRIDGE PINOUT = https://drive.google.com/file/d/1--dudUWrGzpyWKEEIy7zS6vQjkBpHAVd/view?usp=sharing
 
-#Configuración interfaz IO
+#OUTPUT CONFIGURATION 
 
 ENA=PWM(Pin(21), freq=1000, duty=0)
 IN1=Pin(15, Pin.OUT, value=0)
@@ -14,66 +15,66 @@ IN3=Pin(5, Pin.OUT, value=0)
 IN4=Pin(18, Pin.OUT, value=0)
 ENB=PWM(Pin(19), freq=1000, duty=0)
 
-#funciones
+#FUNCTIONS
 
-def adelante():
+def go():
     ENA.duty(512)
     ENB.duty(512)
     IN1.on()
     IN2.off()
     IN3.on()
     IN4.off()
-    print('Hacia adelante')
-    sleep(10)
-    frena()
+    print('To go ahead')
+    sleep(12)
+    stop()
     
     
 
-def atras():
+def back():
     ENA.duty(512)
     ENB.duty(512)
     IN1.off()
     IN2.on()
     IN3.off()
     IN4.on()
-    print('Hacia atras')
+    print('Go back')
     sleep(10)
-    frena()
+    stop()
     
 
-def gir_der():
+def spin_right():
     ENA.duty(512)
     ENB.duty(512)
     IN1.off()
     IN2.on()
     IN3.on()
     IN4.off()
-    print('Hacia atras')
+    print('spin right')
     sleep(10)
-    frena()
+    stop()
     
 
-def gir_izq():
+def spin_left():
     ENA.duty(512)
     ENB.duty(512)
     IN1.on()
     IN2.off()
     IN3.off()
     IN4.on()
-    print('Hacia atras')
+    print('spin left')
     sleep(10)
-    frena()
+    stop()
        
     
 
-def frena():
+def stop():
     ENA.duty(512)
     ENB.duty(512)   
     IN1.off()
     IN2.off()
     IN3.off()
     IN4.off()
-    print('motores detenidos')
+    print('Stopped')
 
 
 
